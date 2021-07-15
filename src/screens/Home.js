d5aa960e59af2845b19ba2b899dada8dd473380e
hyun-jip <frank910@naver.com>
2021-07-15 18:32:44 +0900

    21-07-15

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,6 +1,10 @@
 import { logUserOut } from "../apollo";
+import routes from "../routes";
+import { useHistory } from "react-router-dom";
 
 function Home() {
+  const history = useHistory();
+  history.replace(routes.home, null);
   return (
     <div>
       <h1>Welcome we did it!</h1>
